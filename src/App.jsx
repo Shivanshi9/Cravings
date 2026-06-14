b09c965ccc08818d6a29ecf89af1660d9830d465
Shivanshi9 <shivanshisugandhi@gmail.com>
2026-06-14 14:10:09 +0530

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => {
   return (
 <>
-    <div>App</div>
+    <BrowserRouter>
+    <Header />
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer /> 
+    </BrowserRouter>
+    
 </>
   )
 }
